fa2d1b4c8dc2096200f460052eb734e00bcb5eda
marto ndegz <martondegz@gmail.com>
2016-08-15 12:07:51 +0300

    updated

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-$(document).foundation()
+TweenLite.from(document.getElementById("intro"), 3, {
+    x: 100,
+    ease: Cubic.easeOut
+});
+TweenLite.from(document.getElementById("mail"), 3, {
+    y: 100,
+    ease: Cubic.easeOut
+});
+TweenLite.from(document.getElementById("myself"), 5, {
+    opacity: 0,
+    scale: 0,
+    padding: 100,
+    ease: Cubic.easeOut
+})
