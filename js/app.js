dd0775502c58f76b44c74007560cd9ce6fc134f1
marto ndegz <martondegz@gmail.com>
2016-08-15 15:53:09 +0300

    updated

--- js/app.js
+++ js/app.js
@@ -4,11 +4,21 @@ TweenLite.from(document.getElementById("intro"), 3, {
 });
 TweenLite.from(document.getElementById("mail"), 3, {
     y: 100,
-    ease: Cubic.easeOut
+    ease: Linear.easeOut
 });
 TweenLite.from(document.getElementById("myself"), 5, {
     opacity: 0,
     scale: 0,
     padding: 100,
     ease: Cubic.easeOut
+})
+var h2 = document.getElementsByTagName('h2');
+TweenLite.from(h2, 5, {
+    opacity: 0,
+    y: 50
+})
+var form = document.getElementsByTagName('form');
+TweenLite.from(form, 3, {
+    autoAlpha: 0,
+    delay: 5
 })
